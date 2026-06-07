2f0c411a5a162fcf2e299d9e9bb3d9b861f02c10
SamuK-Code <samuel30nelk@gmail.com>
2026-06-07 14:10:35 -0300

    Add files via upload

---
+++ src/utils/InteractionManagerPatch.js
@@ -0,0 +1,13 @@
+import { InteractionManager } from 'react-native';
+
+// Substitui o runAfterInteractions por setTimeout se necessário
+const originalRunAfterInteractions = InteractionManager.runAfterInteractions;
+
+InteractionManager.runAfterInteractions = (task) => {
+  if (typeof task === 'function') {
+    return originalRunAfterInteractions(task);
+  }
+  return { cancel: () => {} };
+};
+
+export default InteractionManager;
