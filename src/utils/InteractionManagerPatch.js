d71c179d889288638351298d67508955722e2531
SamuK-Code <samuel30nelk@gmail.com>
2026-06-16 09:08:45 -0300

    Update InteractionManagerPatch.js

--- src/utils/InteractionManagerPatch.js
+++ src/utils/InteractionManagerPatch.js
@@ -1,4 +1,5 @@
 import { InteractionManager } from 'react-native';
+import * as Haptics from 'expo-haptics';
 
 const originalRunAfterInteractions = InteractionManager.runAfterInteractions;
 
@@ -7,6 +8,34 @@ InteractionManager.runAfterInteractions = (task) => {
     return originalRunAfterInteractions(task);
   }
   return { cancel: () => {} };
+};
+
+export const triggerHaptic = async (type = 'light') => {
+  try {
+    switch (type) {
+      case 'success':
+        await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+        break;
+      case 'warning':
+        await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning);
+        break;
+      case 'error':
+        await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+        break;
+      case 'medium':
+        await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+        break;
+      case 'heavy':
+        await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+        break;
+      case 'light':
+      default:
+        await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+        break;
+    }
+  } catch (error) {
+    console.log('Haptic failed:', error);
+  }
 };
 
 export default InteractionManager;
