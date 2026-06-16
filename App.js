c56292e15b374e1cf15b0cd0818742193d1e8396
SamuK-Code <samuel30nelk@gmail.com>
2026-06-16 09:06:59 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@
 
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
